c9c4aa9dade523b7585eb501b4bde7c22d3687bf
Jonathan Bayer <linuxgurugamer@gmail.com>
2023-05-25 10:52:03 -0400

    Updated version file

--- RecoveryController/AssemblyVersion.cs
+++ RecoveryController/AssemblyVersion.cs
@@ -10,6 +10,6 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("0.0.4.0")]
- [assembly: AssemblyFileVersion("0.0.4.0")]
+ [assembly: AssemblyVersion("0.0.4.1")]
+ [assembly: AssemblyFileVersion("0.0.4.1")]
 
